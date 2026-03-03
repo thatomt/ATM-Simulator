82b2ab82f4ad77dcd99cea6a1f444529d0fd497a
Thato M <thatomsekele@gmail.com>
2026-03-03 22:56:04 +0200

    Update atm_simulator.py

--- atm_simulator.py
+++ atm_simulator.py
@@ -4,5 +4,10 @@ def check_balance(balance):
 def deposit(balance):
     amount = float(input("Enter amount to deposit: "))
     return balance + amount
+    
 def withdraw(balance):
     amount = float(input("Enter amount to withdraw: "))
+    if amount > balance:
+        print("Insufficient funds")
+        return balance
+    return balance - amount
